27076084e062513b17f9fd5716eabc790a19ea03
walkerlee1202 <walker1202@gmail.com>
2023-05-13 09:58:03 +0800

    update

--- try_gcs_tool.py
+++ try_gcs_tool.py
@@ -1,14 +1,14 @@
 from gcs_tool import gcs_upload, gcs_download
 
 local_path = './testfolder/'
-bucket_name = 'queue_size'
+bucket_name = 'test_bucket'
 destination_path = ''
 source_path = 'ccc/'
 local_destination_path = 'aaa/bb'
 
 service_account_json = 'env/sa.json'
 
-# gcs_upload(local_path, bucket_name, destination_path, service_account_json)
+gcs_upload(local_path, bucket_name, destination_path, service_account_json)
 
 gcs_download(bucket_name, source_path, local_destination_path, service_account_json)
 
